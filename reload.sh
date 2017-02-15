9a78c2127705ea7d7ba48d697b6e9a9042a60a4d
csdiedrich <csdiedrich@gmail.com>
2017-02-15 13:39:44 -0200

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sed -i 's/cfg_file=\/opt\/nagios\/etc\/objects\/localhost.cfg/#cfg_file=\/opt\/nagios\/etc\/objects\/localhost.cfg/g' /opt/nagios/etc/nagios.cfg
 ps -ef|grep nagios|grep opt|awk '{print $2}'|xargs kill -9
